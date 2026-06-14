0b0f2d0f872bc4d4216a35e33be625e600de2767
nazozokc <nazozokc@gmail.com>
2026-06-14 13:55:14 +0900

    chore: add typos spell check
    - Create typos.toml config excluding pnpm-lock.yaml and dist/
    - Add lint:typos script to package.json
    - Add typos step to CI (Linux only, uses crate-ci/typos action)
    - Add Nix flake devShell with typos, nodejs, pnpm

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "subtrack";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            name = "subtrack-dev";
+            packages = with pkgs; [
+              typos
+              nodejs
+              pnpm
+            ];
+            shellHook = ''
+              echo "[subtrack devShell] typos available"
+            '';
+          };
+        };
+    };
+}
